f6d0f696a380877dedfd2e0bd896a430e3648087
max_liuhu <ecustbaihu@163.com>
2018-10-30 20:32:25 +0800

    Update fluent Python/16.Coroutines/16.7yield from.py

---
+++ fluent Python/16.Coroutines/16.7yield from.py
@@ -0,0 +1,59 @@
+from collections import namedtuple
+
+Result = namedtuple('Result', 'count average')
+
+
+# 子生成器
+def averager():
+    total = 0.0
+    count = 0
+    average = None
+    while True:
+        term = yield
+        if term is None:
+            break
+        total += term
+        count += 1
+        average = total / count
+    return Result(count, average)
+
+
+# 委派生成器
+def grouper(results, key):
+    while True:
+        results[key] = yield from averager()
+
+
+# 客户端代码，即调用方
+def main(data):
+    results = {}
+    for key, values in data.items():
+        group = grouper(results, key)
+        next(group)
+        for value in values:
+            group.send(value)
+        group.send(None)  # 重要！
+    print(results)  # 如果要调试，去掉注释
+    report(results)
+
+
+# 输出报告
+def report(results):
+    for key, result in sorted(results.items()):
+        group, unit = key.split(';')
+        print('{:2} {:5} averaging {:.2f}{}'.format(result.count, group, result.average, unit))
+
+
+data = {
+    'girls;kg':
+        [40.9, 38.5, 44.3, 42.2, 45.2, 41.7, 44.5, 38.0, 40.6, 44.5],
+    'girls;m':
+        [1.6, 1.51, 1.4, 1.3, 1.41, 1.39, 1.33, 1.46, 1.45, 1.43],
+    'boys;kg':
+        [39.0, 40.8, 43.2, 40.8, 43.1, 38.6, 41.4, 40.6, 36.3],
+    'boys;m':
+        [1.38, 1.5, 1.32, 1.25, 1.37, 1.48, 1.25, 1.49, 1.46],
+}
+
+if __name__ == '__main__':
+    main(data)
