07725b5eccb4f4ec865ccec81add36007652bf4c
max_liuhu <ecustbaihu@163.com>
2018-11-11 01:01:43 +0800

    Update fluent Python/asyncio并发编程/test_call_soon.py

---
+++ fluent Python/asyncio并发编程/test_call_soon.py
@@ -0,0 +1,18 @@
+import asyncio
+
+
+def callback(sleep_times):
+    print('sleep {} suceess'.format(sleep_times))
+
+
+def stoploop(loop):
+    loop.stop()
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    # 消息队列中立即执行
+    loop.call_soon(callback, 2)
+    loop.call_soon(stoploop, loop)
+    # 这里使用run_forever而不是until_completed,因为调用的是函数而不是协程
+    loop.run_forever()
