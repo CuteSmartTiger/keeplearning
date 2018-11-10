74c564a8441e993040a9a445306e7bea9d51efcf
max_liuhu <ecustbaihu@163.com>
2018-11-11 01:01:58 +0800

    Update fluent Python/asyncio并发编程/coroutine_nest2.py

---
+++ fluent Python/asyncio并发编程/coroutine_nest2.py
@@ -0,0 +1,21 @@
+import asyncio
+import time
+
+# 协程的嵌套，参考官方文档：https://docs.python.org/3.6/library/asyncio-task.html
+import asyncio
+
+
+async def compute(x, y):
+    print("Compute %s + %s ..." % (x, y))
+    await asyncio.sleep(1.0)
+    return x + y
+
+
+async def print_sum(x, y):
+    result = await compute(x, y)
+    print("%s + %s = %s" % (x, y, result))
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(print_sum(1, 2))
+loop.close()
