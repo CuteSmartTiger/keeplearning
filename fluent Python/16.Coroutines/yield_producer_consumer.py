9e2d94aa56599ce332e7b0b0db3f7d1d3a9b8049
max_liuhu <ecustbaihu@163.com>
2018-11-12 23:22:11 +0800

    Update fluent Python/16.Coroutines/yield_producer_consumer.py

---
+++ fluent Python/16.Coroutines/yield_producer_consumer.py
@@ -0,0 +1,25 @@
+# 整个流程无锁，由一个线程执行，produce和consumer协作完成任务，所以称为“协程”，而非线程的抢占式多任务。
+def consumer():
+    r = ''
+    while True:
+        n = yield r
+        if not n:
+            return
+        print('[CONSUMER] Consuming %s...' % n)
+        r = '200 OK'
+
+
+def produce(c):
+    c.send(None)
+    print('starting')
+    n = 0
+    while n < 5:
+        n += 1
+        print('[PRODUCER] Producing %s...' % n)
+        r = c.send(n)
+        print('[PRODUCER] Consumer return: %s' % r)
+    c.close()
+
+
+c = consumer()
+produce(c)
