60d9208cc8cedda595621f8b5323ed42a0c8eac1
max_liuhu <ecustbaihu@163.com>
2018-11-14 13:25:31 +0800

    Update fluent Python/魔法函数/getattribute.py

--- fluent Python/魔法函数/getattribute.py
+++ fluent Python/魔法函数/getattribute.py
@@ -7,10 +7,16 @@ class School(object):
         return None
 
     def __getattribute__(self, item):
-        return '无条件调用属性接口'
+        raise AttributeError
+        # return '无条件调用属性接口'
 
 
 if __name__ == '__main__':
     school = School(['A', 'B', 'C'])
     print(school.age)
     print(school.students)
+
+# __getattribute__(self, name)尝试获取指定的属性时总会调用这个方法，不过，寻找的属性是特
+#    殊属性或特殊方法时除外。点号与 getattr 和 hasattr 内置函数会触发这个方法。调用 __getattribute__
+#    方法且抛出 AttributeError异常时，才会调用 __getattr__ 方法。为了在获取 obj 实例的属性时不导致无
+#    限递归，__getattribute__ 方法的实现要使用super().__getattribute__(obj, name)。
