e9bc18e51ebdc0e22c8699017872286efaa5dd81
max_liuhu <ecustbaihu@163.com>
2019-01-07 18:44:36 +0800

    Create naive_server.py

---
+++ Gevent/Python服务端编程Gevent框架视频教程/15gevent.server使用/naive_server.py
@@ -0,0 +1,32 @@
+import socket
+import sys
+
+HOST = ‘localhost'   # Symbolic name, meaning all available interfaces
+PORT = 5000 # Arbitrary non-privileged port
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
+
+#Bind socket to local host and port
+try:
+    s.bind((HOST, PORT))
+except socket.error as msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+
+print 'Socket bind complete'
+
+#Start listening on socket
+s.listen(100)
+print 'Socket now listening'
+
+#now keep talking with the client
+while 1:
+    #wait to accept a connection - blocking call
+    conn, addr = s.accept()
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+    data = conn.recv(1024)
+    print data
+    conn.close()
+
+s.close()
