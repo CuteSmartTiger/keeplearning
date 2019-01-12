3889f56350688783e00e04d053a41f85dd78df92
max_liuhu <ecustbaihu@163.com>
2019-01-12 22:52:05 +0800

    Create 03多个消费者按顺序取消息之rabbitmq_mutilconsume2.py

---
+++ 消息队列/Python相关操作rabbitmq/03多个消费者按顺序取消息之rabbitmq_mutilconsume2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/1/12 16:34
+# @Author  : liuhu
+# @Site    : 
+# @File    : 03多个消费者按顺序取消息之rabbitmq_consume.py
+# @Software: PyCharm
+# @github  :https://github.com/Max-Liuhu
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'liuhu'
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.37.134'))
+channel = connection.channel()
+
+# make message persistent
+channel.queue_declare(queue='mutil_consume')
+
+
+def callback(ch, method, properties, body):
+    print(" [x2] Received %r" % body)
+    import time
+    time.sleep(5)
+    print('ok')
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.basic_qos(prefetch_count=1)
+
+channel.basic_consume(callback,
+                      queue='mutil_consume',
+                      no_ack=False)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
