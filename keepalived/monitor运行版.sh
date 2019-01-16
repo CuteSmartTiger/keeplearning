0a8abb74361e673d55deab84ee8aa01890fcefa3
MAX-liuhu <max_liuhu@163.com>
2019-01-16 09:49:48 +0800

    Create monitor运行版.sh

---
+++ keepalived/monitor运行版.sh
@@ -0,0 +1,128 @@
+#!/bin/bash   
+
+# 获取ip的个数并输出
+ip_nums=$(echo ` ip addr | grep 'global eth0' | awk 'BEGIN {count=0;} {count=count+1;} END {print count}'`)
+echo ${ip_nums}
+
+
+if [ "${ip_nums}" == "1" ];
+then
+    file=/opt/float
+    if [ ! -f "$file" ]; then
+        touch "$file"  
+        echo 0 > "$file"
+    fi
+
+    value=$(echo `cat /opt/float`)
+    echo ${value}
+    if [ "${value}" == "8" ];then
+        echo 'reset tag'
+        echo 0 > ${file}
+    fi
+
+    #监控desktop
+
+
+    if [ "$(ps -ef | grep "desktop" | grep -v grep)" == "" ]
+    then
+        if [ "$(ps -ef | grep "mysqld" | grep -v grep)" == "" ]; then
+            echo `date`  "mysql stop" >> /var/log/messages
+            exit 2    
+
+        elif [ "$(ps -ef | grep "redis"| grep -v grep)" == "" ]; then
+            echo `date`   "redis stop" >> /var/log/messages
+            exit 3
+
+        elif [ "$(ps -ef | grep "etcd" | grep -v grep)" == "" ]; then
+            echo `date` "etcd stop" >> /var/log/messages
+            exit 6
+
+        elif [ "$(ps -ef | grep "websock" | grep -v grep)" == "" ]; then
+            echo `date` "etcd stop" >> /var/log/messages
+            exit 7
+        else
+            echo `date` "died container"
+            exit 0
+        fi
+    else
+        #杀死desktop容器
+        docker kill vdidesktop-desktop
+        echo `date` 'kill container'
+        exit 0
+    fi
+fi
+
+if [ "${ip_nums}" == "2" ]
+then
+    #判断desktop是否启动
+    if [ "$(ps -ef | grep "desktop" | grep -v grep)" == "" ]
+    then
+        if [ "$(ps -ef | grep "mysqld" | grep -v grep)" == "" ]; then
+            echo `date` "mysql stop" >> /var/log/messages
+            exit 2    
+
+        elif [ "$(ps -ef | grep "redis"| grep -v grep)" == "" ]; then
+            echo `date` "redis stop" >> /var/log/messages
+            exit 3
+
+        elif [ "$(ps -ef | grep "etcd" | grep -v grep)" == "" ]; then
+            echo `date` "etcd stop" >> /var/log/messages
+            exit 6
+
+        elif [ "$(ps -ef | grep "websock" | grep -v grep)" == "" ]; then
+            echo `date` "etcd stop" >> /var/log/messages
+            exit 7
+        else
+            #如果是第一次漂移则启动容器,则设置初始值0，如果不是则返回数字表示容器异常
+            file=/opt/float
+            if [ ! -f "$file" ]; then
+               touch "$file"
+               echo 0 > "$file"
+            fi
+
+            #如果file内容为0则代表刚飘逸过来
+            value=$(echo `cat /opt/float | awk '{print $1 }'`)
+            echo ${value}
+            if [ "${value}" == 0 ]
+            then
+                docker start vdidesktop-desktop
+                echo 8 > "$file"
+                echo `date` "docker start vdidesktop"
+                exit 0
+            else
+                echo `date` "vdidesktop stop" >> /var/log/messages
+                exit 4
+            fi
+        fi
+    else
+        if [ "$(ps -ef | grep "mysqld" | grep -v grep)" == "" ]; then
+            echo `date` "mysql stop" >> /var/log/messages
+            exit 2    
+
+        elif [ "$(ps -ef | grep "redis"| grep -v grep)" == "" ]; then
+            echo `date` "redis stop" >> /var/log/messages
+            exit 3
+
+        elif [ "$(ps -ef | grep "etcd" | grep -v grep)" == "" ]; then
+            echo `date` "etcd stop" >> /var/log/messages
+            exit 6
+
+        elif [ "$(ps -ef | grep "websock" | grep -v grep)" == "" ]; then
+            echo `date` "etcd stop" >> /var/log/messages
+            exit 7
+        else
+            sleep 40
+            if [ "$(ps -ef | grep "nginx: master process"| grep -v grep )" == "" ]; then
+                echo `date` "nginx stop" >> /var/log/messages
+                exit 1
+
+            elif [ "$(ps -ef | grep "salt" | grep -v grep)" == "" ]; then
+                echo `date` "salt stop" >> /var/log/messages
+                exit 5
+            else
+                echo `date` 'container alive' >> /var/log/message
+                exit 0
+            fi      
+        fi
+    fi
+fi
