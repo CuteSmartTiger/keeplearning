ba2cb4eb29d3a27da38fc86ba00cd33d20325642
MAX-liuhu <max_liuhu@163.com>
2019-02-25 17:41:03 +0800

    Create __init__.py

---
+++ redis/python_redis/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/2/25 11:41
+# @Author  : liuhu
+# @File    : __init__.py
+# @Software: PyCharm
+# @github  :https://github.com/Max-Liuhu
+
