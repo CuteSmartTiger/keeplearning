07fa7a79723f6e0331c7ae2f99bcbe6a5426b47d
MAX-liuhu <max_liuhu@163.com>
2019-03-29 15:59:03 +0800

    Create 行为型模式之命令模式.py

---
+++ 系统设计与高并发/模式设计/行为型模式之命令模式.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/3/29 13:50
+# @Author  : liuhu
+# @File    : 行为型模式之命令模式.py
+# @Software: PyCharm
+# @github  :https://github.com/Max-Liuhu
+
+
+import os
+
+verbose = True
+
+
+class RenameFile:
+    def __init__(self, path_src, path_dest):
+        self.src, self.dest = path_src, path_dest
+
+    def execute(self):
+        if verbose:
+            print("[renaming '{}' to '{}']".format(self.src, self.dest))
+        os.rename(self.src, self.dest)
+
+    def undo(self):
+        if verbose:
+            print("[renaming '{}' back to '{}']".format(self.dest, self.src))
+        os.rename(self.dest, self.src)
+
+
+class CreateFile:
+    def __init__(self, path, txt='hello world\n'):
+        self.path, self.txt = path, txt
+
+    def execute(self):
+        if verbose:
+            print("[creating file '{}']".format(self.path))
+        with open(self.path, mode='w') as out_file:
+            out_file.write(self.txt)
+
+    def undo(self):
+        delete_file(self.path)
+
+
+class ReadFile:
+    def __init__(self, path):
+        self.path = path
+
+    def execute(self):
+        if verbose:
+            print("[reading file '{}']".format(self.path))
+        with open(self.path, mode='r') as in_file:
+            print(in_file.read())
+
+
+def delete_file(path):
+    if verbose:
+        print("deleting file '{}'".format(path))
+    os.remove(path)
+
+
+def main():
+    orig_name, new_name = 'file1', 'file2'
+    commands = []
+    for cmd in CreateFile(orig_name), ReadFile(orig_name), RenameFile(orig_name, new_name):
+        commands.append(cmd)
+        [c.execute() for c in commands]
+        answer = input('reverse the executed commands? [y/n] ')
+    if answer not in 'yY':
+        print("the result is {}".format(new_name))
+        exit()
+    for c in reversed(commands):
+        try:
+            c.undo()
+        except AttributeError as e:
+            pass
+
+
+if __name__ == "__main__":
+    main()
