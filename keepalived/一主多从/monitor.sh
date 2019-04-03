277cc229bdad9410efe56a0c50ea460a982433f9
MAX-liuhu <max_liuhu@163.com>
2019-04-03 20:09:33 +0800

    Update monitor.sh

--- keepalived/一主多从/monitor.sh
+++ keepalived/一主多从/monitor.sh
@@ -1,17 +1,35 @@
 
 ```shell
 #!/bin/bash
+
+# 定义日志存储的文件
 log_file=/var/log/keepalive.log
 
 function record_log() {
-  #statements
+  #记录日志
   echo `date "+%Y-%m-%d %H:%M:%S"`  $1 has stopped >> $log_file
   exit $2
 }
 
+
+# 检查虚拟ip是否存在，不存在则重启keepalived服务
+function check_virtual_ip() {
+vir_ip=$(echo `egrep '([0-9]{1,3}\.){1,3}[0-9]{1,3}' /etc/keepalived/keepalived.conf | awk '{ print $1 }'`)
+
+ping_ip=$(echo `ping -c 1  ${vir_ip} | grep "packet loss" | awk '{ print $4 }'`)
+
+if [ "${ping_ip}"x == "0"x ];then
+  `service keepalived restart`
+  exit 1
+else
+  exit 0
+fi
+}
+
+
 if [ "$(ps -ef | grep vdidesktop | grep -v grep | grep -v  mysql)" == "" ]
 then
-    record_log vdidesktop-desktop 1
+    `record_log vdidesktop-desktop 1`
 else
     if [ "$(ps -ef | grep "mysqld" | grep -v grep)" == "" ]; then
         record_log vdidesktop-mysql 2
@@ -27,7 +45,12 @@ else
         elif [ "$(ps -ef | grep "salt" | grep -v grep)" == "" ]; then
             record_log salt 7
         else
+          check_virtual_ip
+          if [ "$?" == "0" ];then
             record_log 'all containers alive,and None' 0
+          else
+            record_log 'all containers alive,but keepalived' 0
+          fi
         fi
     fi
 fi
