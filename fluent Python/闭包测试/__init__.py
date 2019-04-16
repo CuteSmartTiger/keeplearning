31301d58f06c18be1c580bd7c1a50ccd7a52017e
max_liuhu <ecustbaihu@163.com>
2019-04-16 19:16:24 +0800

    Create __init__.py

---
+++ fluent Python/闭包测试/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/4/11 0:09
+# @Author  : liuhu
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
+# @github  :https://github.com/Max-Liuhu
