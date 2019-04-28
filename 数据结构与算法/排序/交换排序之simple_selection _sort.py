e7b4615b0db2290d7613f3ab118804647659ac4f
max_liuhu <ecustbaihu@163.com>
2019-04-28 16:32:03 +0800

    Update 交换排序之simple_selection _sort.py

--- 数据结构与算法/排序/交换排序之simple_selection _sort.py
+++ 数据结构与算法/排序/交换排序之simple_selection _sort.py
@@ -6,37 +6,21 @@
 # @Software: PyCharm
 # @github  :https://github.com/Max-Liuhu
 
-from __future__ import print_function
 
-
-# 此方法应该为简单排序，而不是插入排序
-def simple_selection_sort(collection):
-    """Pure implementation of the simple selection  sort algorithm in Python
-    :param collection: some mutable ordered collection with heterogeneous
-    comparable items inside
-    :return: the same collection ordered by ascending
-    Examples:
-    >>> simple_selection_sort([0, 5, 3, 2, 2])
-    [0, 2, 2, 3, 5]
-    >>> simple_selection_sort([])
-    []
-    >>> simple_selection_sort([-2, -5, -45])
-    [-45, -5, -2]
+def simple_select_sort(unsorted_list):
     """
-    for index in range(1, len(collection)):
-        while index > 0 and collection[index - 1] > collection[index]:
-            collection[index], collection[index - 1] = collection[index - 1], collection[index]
-            index -= 1
+    :param unsorted_list: list
+    :return:
+    """
+    length = len(unsorted_list)
+    for i in range(length - 1):
+        index_of_min_value = i
+        for j in range(i + 1, length):
+            if unsorted_list[j] < unsorted_list[index_of_min_value]:
+                index_of_min_value = j
+        unsorted_list[index_of_min_value], unsorted_list[i] = unsorted_list[i], unsorted_list[index_of_min_value]
+    print(unsorted_list)
+    return unsorted_list
 
-    return collection
 
-
-if __name__ == '__main__':
-    try:
-        raw_input          # Python 2
-    except NameError:
-        raw_input = input  # Python 3
-
-    user_input = raw_input('Enter numbers separated by a comma:\n').strip()
-    unsorted = [int(item) for item in user_input.split(',')]
-    print(simple_selection_sort(unsorted))
+simple_select_sort([0, 5, 3, 2, 2])
